827a688087dd13b76ee00c32938ceb3b170d99df
Laura Hoffman <lhoffmanwg@gmail.com>
2019-02-05 05:55:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   new_array = []
   array.each do |x|
-  squared = x ** 2
-  new_array.push(squared)
+    squared = x ** 2
+    new_array.push(squared)
   end  
+  return new_array
 end
